2040a48cdcc1b125089b6dc1f762c1622bba190b
Bowuigi <70549864+Bowuigi@users.noreply.github.com>
2021-05-21 15:35:03 -0300

    Bar is now back to the top

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const unsigned int gappx     = 3;       /* default gap between windows in
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_white[]       = "#ffffff";
